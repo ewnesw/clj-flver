2d78f0a6166d73a1d733633de4811d083c511aa5
ewnesw <mlucas@hexagon.al>
2023-09-18 01:49:01 +0200

    added atom paths and multiple directory path management

--- src/clj_flver/core.clj
+++ src/clj_flver/core.clj
@@ -1,18 +1,30 @@
 (ns clj-flver.core
   (:require [clojure-watch.core :refer [start-watch]]))
 
-(defn add-file
+(def paths (atom {}))
+
+(defn watch-path
   "take a path and start watching the given path"
   [path]
-  ((start-watch [{:path path
+  (start-watch [{:path path
                :event-types [:create :modify :delete]
                :bootstrap (fn [path] (println "Starting to watch " path))
-               :callback (fn [event filename] (println event filename))
-               :options {:recursive true}}])))
+               :callback (fn [event filename] (println "proc" event filename))
+               :options {:recursive true}}]))
+
+(defn add-path
+  "add given path and return fn from watch-path to atom paths"
+  [path]
+  (swap! paths
+         (fn [current-state]
+           (assoc current-state path (watch-path path)))))
+
+(defn it-path
+  "iterate and apply add-path"
+  [lpaths]
+  (map add-path lpaths))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (add-file (first args)))
-
-;;(main "/home/laiminux/.bash_aliases")
+  (dorun (map println (it-path args))))
