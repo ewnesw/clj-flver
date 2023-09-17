0f0f35a0f8e66ff9b32b86668e41875d723b5c7f
ewnesw <mlucas@hexagon.al>
2023-09-17 22:44:02 +0200

    added clojure-watch dependencies && one path watch

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [clojure-watch "LATEST"]]
   :main ^:skip-aot clj-flver.core
   :target-path "target/%s"
+  :plugins [[cider/cider-nrepl "0.24.0"]]
   :profiles {:uberjar {:aot :all}})
