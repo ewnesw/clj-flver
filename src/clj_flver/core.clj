e81759e67a43502f89aeb20b519d4c63a53978fb
ewnesw <mlucas@hexagon.al>
2023-09-17 22:44:02 +0200

    added clojure-watch dependencies && one path watch

--- src/clj_flver/core.clj
+++ src/clj_flver/core.clj
@@ -1,7 +1,18 @@
 (ns clj-flver.core
-  (:gen-class))
+  (:require [clojure-watch.core :refer [start-watch]]))
+
+(defn add-file
+  "take a path and start watching the given path"
+  [path]
+  ((start-watch [{:path path
+               :event-types [:create :modify :delete]
+               :bootstrap (fn [path] (println "Starting to watch " path))
+               :callback (fn [event filename] (println event filename))
+               :options {:recursive true}}])))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (add-file (first args)))
+
+;;(main "/home/laiminux/.bash_aliases")
